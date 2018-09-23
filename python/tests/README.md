a8e6948b93cb05793518b3ef8487b6d9a82d3195
James Little <james@ergo.kiwi>
2018-09-23 21:19:23 +1200

    Extended test coverage and added instructions to get started

---
+++ python/tests/README.md
@@ -0,0 +1,7 @@
+# Python unittest
+
+These appear to run fine when ran as script. For example
+
+```bash
+ python -m unittest code-kata/python/tests/test-kata02-binary-search.py in code-kata/python
+```
--- python/tests/test-kata02-binary-search.py
+++ python/tests/test-kata02-binary-search.py
@@ -3,8 +3,8 @@ from .context import code_kata
 import unittest
 
 
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
+class Kata02TestSuite(unittest.TestCase):
+    """Basic test cases for Kata 02 - the karate chop."""
 
     def test_empty_array(self):
         self.assertEqual(-1, code_kata.chop(3, []))
@@ -15,12 +15,23 @@ class BasicTestSuite(unittest.TestCase):
 
     def test_3_items(self):
         self.assertEqual(0, code_kata.chop(1, [1, 3, 5]))
-        self.assertEqual(1, code_kata.chop(2, [1, 3, 5]))
-        self.assertEqual(2, code_kata.chop(3, [1, 3, 5]))
+        self.assertEqual(1, code_kata.chop(3, [1, 3, 5]))
+        self.assertEqual(2, code_kata.chop(5, [1, 3, 5]))
         self.assertEqual(-1, code_kata.chop(0, [1, 3, 5]))
         self.assertEqual(-1, code_kata.chop(2, [1, 3, 5]))
         self.assertEqual(-1, code_kata.chop(4, [1, 3, 5]))
         self.assertEqual(-1, code_kata.chop(6, [1, 3, 5]))
+
+    def test_4_items(self):
+        self.assertEqual(0, code_kata.chop(1, [1, 3, 5, 7]))
+        self.assertEqual(1, code_kata.chop(3, [1, 3, 5, 7]))
+        self.assertEqual(2, code_kata.chop(5, [1, 3, 5, 7]))
+        self.assertEqual(3, code_kata.chop(7, [1, 3, 5, 7]))
+        self.assertEqual(-1, code_kata.chop(0, [1, 3, 5, 7]))
+        self.assertEqual(-1, code_kata.chop(2, [1, 3, 5, 7]))
+        self.assertEqual(-1, code_kata.chop(4, [1, 3, 5, 7]))
+        self.assertEqual(-1, code_kata.chop(6, [1, 3, 5, 7]))
+        self.assertEqual(-1, code_kata.chop(8, [1, 3, 5, 7]))
 
 
 if __name__ == '__main__':
