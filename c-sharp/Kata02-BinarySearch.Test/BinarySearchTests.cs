41ff474f510487b1efe543c75278e9f7c749874c
James Little <james@ergo.kiwi>
2018-09-25 22:23:42 +1200

    Added C# CodeKata

---
+++ c-sharp/Kata02-BinarySearch.Test/BinarySearchTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace Kata02_BinarySearch.Test
+{
+    [TestFixture]
+    public class BinarySearchTests
+    {
+        private BinarySearch _binarySearch = new BinarySearch();
+
+        [Test]
+        public void TestEmpty()
+        {
+            Assert.Equals(-1, _binarySearch.Chop(3, new int[] { }));
+        }
+
+        [Test]
+        public void TestSingle()
+        {
+            Assert.Equals(-1, _binarySearch.Chop(3, new[] {1}));
+            Assert.Equals(0, _binarySearch.Chop(1, new[] {1}));
+        }
+
+        [Test]
+        public void TestTriple()
+        {
+            Assert.Equals(0, _binarySearch.Chop(1, new[] {1, 3, 5}));
+            Assert.Equals(1, _binarySearch.Chop(3, new[] {1, 3, 5}));
+            Assert.Equals(2, _binarySearch.Chop(5, new[] {1, 3, 5}));
+            Assert.Equals(-1, _binarySearch.Chop(0, new[] {1, 3, 5}));
+            Assert.Equals(-1, _binarySearch.Chop(2, new[] {1, 3, 5}));
+            Assert.Equals(-1, _binarySearch.Chop(4, new[] {1, 3, 5}));
+            Assert.Equals(-1, _binarySearch.Chop(6, new[] {1, 3, 5}));
+        }
+
+        [Test]
+        public void TestQuadriple()
+        {
+            Assert.Equals(0, _binarySearch.Chop(1, new[] {1, 3, 5, 7}));
+            Assert.Equals(1, _binarySearch.Chop(3, new[] {1, 3, 5, 7}));
+            Assert.Equals(2, _binarySearch.Chop(5, new[] {1, 3, 5, 7}));
+            Assert.Equals(3, _binarySearch.Chop(7, new[] {1, 3, 5, 7}));
+            Assert.Equals(-1, _binarySearch.Chop(0, new[] {1, 3, 5, 7}));
+            Assert.Equals(-1, _binarySearch.Chop(2, new[] {1, 3, 5, 7}));
+            Assert.Equals(-1, _binarySearch.Chop(4, new[] {1, 3, 5, 7}));
+            Assert.Equals(-1, _binarySearch.Chop(6, new[] {1, 3, 5, 7}));
+            Assert.Equals(-1, _binarySearch.Chop(8, new[] {1, 3, 5, 7}));
+        }
+    }
+}
---
+++ c-sharp/Kata02-BinarySearch/BinarySearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kata02_BinarySearch
+{
+    public class BinarySearch
+    {
+        public int Chop(int key, int[] array)
+        {
+            throw new NotImplementedException("Not implemented yet");
+        }
+    }
+}
