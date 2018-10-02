8f0113ac4bf9540dbeee86273b786da743b5a686
James Little <james@ergo.kiwi>
2018-10-02 23:07:49 +1300

    Added recursive solution for erlang

--- erlang/src/kata02_binary_search.erl
+++ erlang/src/kata02_binary_search.erl
@@ -3,5 +3,26 @@
 %% API
 -export([chop/2]).
 
-%% Parameter names must start with an uppercase letter in Erlang?
-chop(Key, Array) -> erlang:error("Not implemented yet!").
+chop(Key, Array) ->
+  Length = length(Array),
+  if
+    Length == 0 -> -1;
+    true ->
+      Mid = length_div_2(Length),
+      Item = lists:nth(Mid, Array),
+      if
+        Item == Key -> Mid - 1;
+        (Length > 1) and (Item > Key) -> chop(Key, lists:sublist(Array, 1, Mid-1));
+        (Length > 1) and (Item < Key) ->
+          Chop = chop(Key, lists:sublist(Array, Mid + 1, Length)),
+          if
+            Chop == -1 -> -1;
+            true -> Mid + Chop
+          end;
+        true -> -1
+      end
+  end.
+
+length_div_2(Length) -> (Length div 2) + 1.
+
+
