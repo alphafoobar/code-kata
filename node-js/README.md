a2c673d95cff5e9278063209717286f5bb05d2f3
James Little <james@ergo.kiwi>
2018-10-02 23:31:01 +1300

    Updated tests to use Promise.all

---
+++ node-js/README.md
@@ -0,0 +1,9 @@
+# Node-JS / JavaScript
+
+If running in WebStorm the Jest test files can be executed from the IDE.
+
+Otherwise tests will run eslint and unit tests.
+
+```
+npm test
+```
--- node-js/test/kata02-binary-search.test.js
+++ node-js/test/kata02-binary-search.test.js
@@ -7,32 +7,42 @@ test('Empty list handled', () => {
     return expect(chop(3, [])).resolves.toEqual(-1);
 });
 
+test('Will Succeed First time', () => {
+    expect.assertions(1);
+    return expect(chop(3, [1, 3, 5])).resolves.toEqual(1);
+});
+
 test('single item lists', () => {
     expect.assertions(2);
-    expect(chop(3, [1])).resolves.toEqual(-1);
-    return expect(chop(1, [1])).resolves.toEqual(0);
+    return Promise.all([
+        expect(chop(3, [1])).resolves.toEqual(-1),
+        expect(chop(1, [1])).resolves.toEqual(0)]);
 });
 
 test('Lists of 3', () => {
     expect.assertions(7);
-    expect(chop(1, [1, 3, 5])).resolves.toEqual(0);
-    expect(chop(3, [1, 3, 5])).resolves.toEqual(1);
-    expect(chop(5, [1, 3, 5])).resolves.toEqual(2);
-    expect(chop(0, [1, 3, 5])).resolves.toEqual(-1);
-    expect(chop(2, [1, 3, 5])).resolves.toEqual(-1);
-    expect(chop(4, [1, 3, 5])).resolves.toEqual(-1);
-    return expect(chop(6, [1, 3, 5])).resolves.toEqual(-1);
+
+    return Promise.all([
+        expect(chop(1, [1, 3, 5])).resolves.toEqual(0),
+        expect(chop(3, [1, 3, 5])).resolves.toEqual(1),
+        expect(chop(5, [1, 3, 5])).resolves.toEqual(2),
+        expect(chop(0, [1, 3, 5])).resolves.toEqual(-1),
+        expect(chop(2, [1, 3, 5])).resolves.toEqual(-1),
+        expect(chop(4, [1, 3, 5])).resolves.toEqual(-1),
+        expect(chop(6, [1, 3, 5])).resolves.toEqual(-1)]);
 });
 
 test('Lists of 4', () => {
     expect.assertions(9);
-    expect(chop(1, [1, 3, 5, 7])).resolves.toEqual(0);
-    expect(chop(3, [1, 3, 5, 7])).resolves.toEqual(1);
-    expect(chop(5, [1, 3, 5, 7])).resolves.toEqual(2);
-    expect(chop(7, [1, 3, 5, 7])).resolves.toEqual(3);
-    expect(chop(0, [1, 3, 5, 7])).resolves.toEqual(-1);
-    expect(chop(2, [1, 3, 5, 7])).resolves.toEqual(-1);
-    expect(chop(4, [1, 3, 5, 7])).resolves.toEqual(-1);
-    expect(chop(6, [1, 3, 5, 7])).resolves.toEqual(-1);
-    return expect(chop(8, [1, 3, 5, 7])).resolves.toEqual(-1);
+
+    return Promise.all([
+        expect(chop(1, [1, 3, 5, 7])).resolves.toEqual(0),
+        expect(chop(3, [1, 3, 5, 7])).resolves.toEqual(1),
+        expect(chop(5, [1, 3, 5, 7])).resolves.toEqual(2),
+        expect(chop(7, [1, 3, 5, 7])).resolves.toEqual(3),
+        expect(chop(0, [1, 3, 5, 7])).resolves.toEqual(-1),
+        expect(chop(2, [1, 3, 5, 7])).resolves.toEqual(-1),
+        expect(chop(4, [1, 3, 5, 7])).resolves.toEqual(-1),
+        expect(chop(6, [1, 3, 5, 7])).resolves.toEqual(-1),
+        expect(chop(8, [1, 3, 5, 7])).resolves.toEqual(-1)]);
 });
