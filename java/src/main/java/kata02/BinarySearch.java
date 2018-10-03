5f8476472476d32ac1656f94a1dbdc55da50b947
Surya <surya@bravenewcoin.com>
2018-10-03 13:55:22 +1300

    BinarySearch Solution 1

--- java/src/main/java/kata02/BinarySearch.java
+++ java/src/main/java/kata02/BinarySearch.java
@@ -3,6 +3,26 @@ package kata02;
 public class BinarySearch {
 
     public int chop(int key, int[] a) {
-        throw new UnsupportedOperationException("not implemented yet");
+
+        int start = 0;
+        int len = a.length;
+        int end = len;
+        int index = (end - start) / 2;
+
+        while (index > 0 && index < len && start != index && end != index) {
+            if (key < a[index]) {
+                end = index;
+                index = (end - start) / 2;
+            } else if (key > a[index]) {
+                start = index;
+                index = index + ((end - start) / 2);
+            } else {
+                break;
+            }
+        }
+        if (len != 0 && key == a[index]) {
+            return index;
+        }
+        return -1;
     }
 }
