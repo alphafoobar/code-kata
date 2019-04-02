600e7b7983752674d9011ff21cb76df186cf0b2f
James Little <james@ergo.kiwi>
2019-04-02 19:53:38 +1300

    Deleting code kata clojure
    Replaced with top level repository at https://github.com/alphafoobar/code-kata-clojure

--- clojure/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject clojure "0.1.0-SNAPSHOT"
-  :description "Code Kata - practising better programming"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot kata02-binary-search.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
